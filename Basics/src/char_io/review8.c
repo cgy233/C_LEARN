61c78e21c642d4416e734ace91161cb665e968b4
cgy233 <1781387847@qq.com>
2021-06-22 16:04:56 +0800

    review8

---
+++ Basics/src/char_io/review8.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+// test1
+void count_char(void);
+// test2
+void char_asci_oct(void);
+// test3
+void count_upper_lower_char(void);
+// test4
+void average_count_word(void);
+// test5
+void guess_plus(void);
+// test6
+void get_first_plus(void);
+// test7
+void wages_plus(void);
+// test8
+void counter(void);
+
+int main(int argc, char const *argv[])
+{	
+	
+	// test1
+	void count_char(void);
+	// test2
+	void char_asci_oct(void);
+	// test3
+	void count_upper_lower_char(void);
+	// test4
+	void average_count_word(void);
+	// test5
+	void guess_plus(void);
+	// test6
+	void get_first_plus(void);
+	// test7
+	void wages_plus(void);
+	// test8
+	void counter(void);
+		
+	return 0;
+}
+
+// test1
+void count_char(void);
+// test2
+void char_asci_oct(void);
+// test3
+void count_upper_lower_char(void);
+// test4
+void average_count_word(void);
+// test5
+void guess_plus(void);
+// test6
+void get_first_plus(void);
+// test7
+void wages_plus(void);
+// test8
+void counter(void);
